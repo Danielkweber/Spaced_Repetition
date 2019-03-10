288d4799ac134cb2f0b565b7a955741de0656385
Daniel Weber <danielkweber1@gmail.com>
2019-03-10 18:49:29 +0200

    Added Pomodoro Timer

---
+++ pomodoro.py
@@ -0,0 +1,9 @@
+import time
+def pomodoro_timer(time):
+    while time > 0:
+        mins, secs = divmod(time, 60)
+        current_timer = '{mins}:{secs}'.format(mins = mins, secs = secs)
+        print(current_timer, end = '\r')
+        time.sleep(1)
+        time -= 1
+    
