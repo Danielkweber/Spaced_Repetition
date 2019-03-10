85f454bbc8ef0cc2a70a674e563c6631d73839cb
Daniel Weber <danielkweber1@gmail.com>
2019-03-10 12:23:53 +0200

    Added Option Tree to main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import pomodoro
+import Repetition_log
+import book_log
+
+def option_list():
+    print('1 - Pomodoro')
+    print('2 - Add Book')
+    print('3 - Add Topic')
+    print('4 - List Topics')
+    print('5 - List Books')
+    user_input = input('Select Option:')
+    if user_input == 1:
+        #pomodoro
+    if user_input == 2:
+        #Book Add
+    if user_input == 3:
+        #Topic Add
+    if user_input == 4:
+        #Topic List
+    if user_input == 5:
+        #booklist
