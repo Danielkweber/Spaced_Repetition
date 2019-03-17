433a726abbd4b3cbf8b14ae94298683b63249d5e
Daniel Weber <danielkweber1@gmail.com>
2019-03-17 08:57:03 +0200

    Removed iscompleted variable, added notes variable, add dates for self.dates_reviewed, added add_notes() and topic_reviewed() methods

--- Repetition_log.py
+++ Repetition_log.py
@@ -1,26 +1,39 @@
-import datetime
+from datetime import date
 import json
 
 class Rep_Log():
-    def __init__(self, subject, topic):
+    def __init__(self, topic, subject):
         self.topic = topic
         self.subject = subject
         self.times_reviewed = 0
-        self.is_completed = False
-        self.dates_reviewed = []
+        #Change Date Format
+        self.dates_reviewed = [date.today().isoformat()]
+        self.notes = {}
+        self.add_notes()
+    def add_notes(self):
+        self.notes[self.times_reviewed] = input('Add Notes: ')
+    def topic_reviewed(self):
+        self.times_reviewed += 1
+        self.dates_reviewed.append(date.today().isoformat())
+        self.add_notes()
     def __repr__(self):
         return repr(self.topic)
     def __str__(self):
-        return "Topic: {self.topic} \n Times Reviewed: {self.times_reviewed} \n Complete: {self.is_completed} \n Review Dates: {self.dates_reviewed}".format(self=self)
+        return "Topic: {self.topic} \n Subject: {self.subject} \n Times Reviewed: {self.times_reviewed} \n Review Dates: {self.dates_reviewed} \n Notes: {self.notes}".format(self=self)
 
-class myJsonEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Rep_Log):
-            return {"topic":obj.topic,"subject":obj.subject,"times_reviewd":obj.times_reviewed,"is_completed":obj.is_completed,"dates_reviewed":obj.dates_reviewed}
-        # Let the base class default method raise the TypeError
-        return json.JSONEncoder.default(self, obj)
+#class myJsonEncoder(json.JSONEncoder):
+#    def default(self, obj):
+#        if isinstance(obj, Rep_Log):
+#            return {"Topic":obj.topic,"Subject":obj.subject,"Times Reviewd":obj.times_reviewed,"Complete":obj.is_completed,"Dates Reviewed":obj.dates_reviewed}
+#        # Let the base class default method raise the TypeError
+#        return json.JSONEncoder.default(self, obj)
+#
+#Science = [Rep_Log('Science',"otherthing"),Rep_Log('Science',"otherthing"),Rep_Log('Science',"otherthing"),Rep_Log('Science',"otherthing")]
+#print(Science)
+#jsons  = json.dumps(Science,cls=myJsonEncoder)
+#print(jsons)
 
-Science = [Rep_Log('Science',"otherthing"),Rep_Log('Science',"otherthing"),Rep_Log('Science',"otherthing"),Rep_Log('Science',"otherthing")]
-print(Science)
-jsons  = json.dumps(Science,cls=myJsonEncoder)
-print(jsons)
+t1 = Rep_Log('Chain', 'Math')
+print(t1)
+t1.topic_reviewed()
+print(t1)
