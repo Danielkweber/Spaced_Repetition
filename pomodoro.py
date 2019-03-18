5f3c26529d8805b5f3eec28507d9b6304201b2b0
Daniel Weber <danielkweber1@gmail.com>
2019-03-18 14:54:58 +0200

    Fixed white space in pomodoro timer

--- pomodoro.py
+++ pomodoro.py
@@ -1,12 +1,12 @@
 import time
 def pomodoro_timer(user_time):
-        user_time *= 60
-        while user_time > 0:
-                mins, secs = divmod(user_time, 60)
-                current_timer = '{mins}:{secs}'.format(mins = mins, secs = secs)
-                print(current_timer, end = '\r')
-                time.sleep(1)
-                user_time -= 1
+	user_time *= 60
+	while user_time > 0:
+		mins, secs = divmod(user_time, 60)
+		current_timer = '{mins}:{secs}'.format(mins = mins, secs = secs)
+		print(current_timer, end = '\r')
+		time.sleep(1)
+		user_time -= 1
 
 
     
